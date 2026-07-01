67a2bb39013384178aff7d6b594cc26b6c5a99b3
Upjung KIM <010timeless@naver.com>
2026-07-01 19:44:16 +0900

    배경 이미지 풍경으로 변경

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './MainContent.css';
+// 1. 이미지 파일 import 추가
+import hoakBackground from './assets/hoak_background_9.png'; 
 
 function MainContent() {
   return (
     <main className="main-content-zone">
+      
+      {/* 2. 좌상단 기준 자동 비율 조절 배경 이미지 추가 */}
+      <img 
+        src={hoakBackground} 
+        alt="House Of Ansan KIM Background" 
+        className="hoak-bg-image"
+      />
+
       {/* 이미지의 Shielding 우측 아래 배치될 세로 나열 문구 상자 */}
       <div className="branding-text-box">
         <p className="brand-item">gracias</p>
